a86fbd3d3da962c892d451d1bf81c255ec86fd32
VitaliiVeshtobei <veshtobey@gmail.com>
2022-10-29 22:25:51 +0300

    fix

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -26,7 +26,10 @@ export const MovieDetails = () => {
         />
         <ContainerInfo>
           <h1>{api.original_title}</h1>
-          <p>User score: {api.popularity.toFixed(0)}%</p>
+          <p>
+            User score:{' '}
+            {api.popularity ? api.popularity.toFixed(0) : 'no popularity'}%
+          </p>
           <h2>Overview</h2>
           <p>{api.overview}</p>
           <h2>Genres</h2>
